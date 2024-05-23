d3e773d8d3b5fe7162152e0cb8fd0176fab1f379
ramimmojumdar <expertramim@gmial.com>
2024-05-23 21:29:57 +0600

    list app

---
+++ app/src/main/java/com/example/listitemapp_listview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.listitemapp_listview;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listView);
+        MyAdapter adapter = new MyAdapter();
+        listView.setAdapter(adapter);
+
+    }
+
+    public class MyAdapter extends BaseAdapter{
+
+        LayoutInflater layoutInflater;
+
+        @Override
+        public int getCount() {
+            return 10;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View myView = layoutInflater.inflate(R.layout.list_item, listView, false);
+
+
+            return myView;
+        }
+    }
+
+}
