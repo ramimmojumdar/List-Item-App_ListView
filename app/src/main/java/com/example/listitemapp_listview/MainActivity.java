92a7eda1b9009c88d00594a4ac795730dd410594
ramimmojumdar <expertramim@gmial.com>
2024-05-24 16:00:42 +0600

    success

--- app/src/main/java/com/example/listitemapp_listview/MainActivity.java
+++ app/src/main/java/com/example/listitemapp_listview/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -17,9 +18,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+    ArrayList<HashMap<String, String>> arrayList = new ArrayList<>();
+    HashMap<String, String> hashMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,16 +36,33 @@ public class MainActivity extends AppCompatActivity {
         MyAdapter adapter = new MyAdapter();
         listView.setAdapter(adapter);
 
+        hashMap = new HashMap<>();
+        hashMap.put("name", "Ramim Mojumdar");
+        hashMap.put("number", "013000000000");
+        arrayList.add(hashMap);
+
+        hashMap = new HashMap<>();
+        hashMap.put("name", "Shmim Mojumdar");
+        hashMap.put("number", "017000000000");
+        arrayList.add(hashMap);
+
+        hashMap = new HashMap<>();
+        hashMap.put("name", "Naim Mojumdar");
+        hashMap.put("number", "016000000000");
+        arrayList.add(hashMap);
+
+
     }
 
     public class MyAdapter extends BaseAdapter{
 
         LayoutInflater layoutInflater;
         LinearLayout itemId;
+//        HashMap<String, String> hashMap;
 
         @Override
         public int getCount() {
-            return 10;
+            return arrayList.size();
         }
 
         @Override
@@ -54,18 +77,25 @@ public class MainActivity extends AppCompatActivity {
 
         @SuppressLint("MissingInflatedId")
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
+        public View getView(int position, View view, ViewGroup viewGroup) {
 
             layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
             View myView = layoutInflater.inflate(R.layout.list_item, listView, false);
             itemId = myView.findViewById(R.id.itemId);
 
+            TextView nameId = myView.findViewById(R.id.nameId);
+            TextView numberId = myView.findViewById(R.id.numberId);
+
+            hashMap = arrayList.get(position);
+            nameId.setText(hashMap.get("name"));
+            numberId.setText(hashMap.get("number"));
+
             itemId.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
 
-                    Toast toast = Toast.makeText(MainActivity.this, "Item number is: "+i, Toast.LENGTH_SHORT);
+                    Toast toast = Toast.makeText(MainActivity.this, "Item number is: "+position, Toast.LENGTH_SHORT);
                     toast.show();
                 }
             });
